1ef898cd56e27a44c0bbb9c68b1575d3ba1d1f77
Jason Fisher <jfisher@usgs.gov>
2016-10-07 14:08:06 -0700

    Fix for scenario 2

--- index.R
+++
@@ -1,9 +0,0 @@
-## ----wrv_pkg_rm, eval=FALSE----------------------------------------------
-## remove.packages("wrv")
-
-## ----inlmisc_pkg_install, eval=FALSE-------------------------------------
-## install.packages("inlmisc", repos = "https://cloud.r-project.org/", dependencies = TRUE)
-
-## ----wrv_pkg_install, eval=FALSE-----------------------------------------
-## install.packages(file.choose(), repos = NULL)
-
